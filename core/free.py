d2118d0189a5c952b6ccf54d623f0fe0ea88417d
Joesv <hesseljoes@gmail.com>
2015-09-30 20:20:34 +0200

    still with an error

---
+++ core/free.py
@@ -0,0 +1,9 @@
+class Free:
+    def __init__(self, place):
+        self.name = "PLACEHOLDER"
+        self.unbreakable = True
+        self.type = "free"
+        self.free = True
+        
+        self.x, self.y = place
+
---
+++ game.py
@@ -0,0 +1,38 @@
+import pygame, sys
+from pygame.locals import *
+from core.player import Player
+from core.walls import Wall
+from core.free import Free
+
+pygame.init()
+fpsClock = pygame.time.Clock()
+
+display = pygame.display.set_mode((1280, 720))
+
+font = pygame.font.SysFont('monospace', 16)
+
+level1 = open('level1.txt')
+level1List = level1.readlines()
+level1Locations = []
+
+for i in range(0, len(level1List)):
+	level1Locations.append([])
+	for j in range(0,len(level1List[i])):
+		
+		letter = j
+		print letter
+		if (level1List[i][j] == "@"):
+			player = Player((i,j))
+			level1Locations[i].append(player)
+		if (level1List[i][j] == "."):
+			free = Free((i,j))
+			level1Locations.append[i](free)
+		if (level1List[i][j] == "#"):
+			wall = Wall((i,j))
+			level1Locations.append[i](wall)	
+		if (level1List[i][j] == "#"):
+			wall = Wall((i,j))
+			level1Locations.append[i](Wall
+
+print level1Locations
+print level1List
