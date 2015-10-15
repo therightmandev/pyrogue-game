874619f49dce15d53fc26587eed1f32bf86d9c0f
BuildTools <salvador.telo@hotmail.com>
2015-10-15 18:45:39 +0100

    stats are pretty good, let's start working on some other stuff now

--- game.py
+++ game.py
@@ -14,6 +14,7 @@ DARK_GREY = (30, 30, 30)
 A_BLUE = (5, 100, 240)
 RED_ORANGE = (245, 34, 5)
 D_BLUE = (0, 179, 255)
+PURPLE = (196, 12, 181)
 
 
 
@@ -49,10 +50,10 @@ class Game(object):
 
         self.render_list = []
         self.tile = pygame.Surface(self.screen.get_size())
-        self.level_text = self.text.text_by_bot_left("Lvl " + str(stats_dict['level']), (self.width/10, self.height/20*19), A_BLUE, self.text.big_font)
-        self.strength_text = self.text.text_by_bot_left("Strenght " + str(stats_dict['strength']), (self.level_text[1].x + self.level_text[1].width, self.height/20*19), A_BLUE, self.text.big_font)
+        self.level_text = self.text.text_by_bot_left("Lvl " + str(stats_dict['level']), (self.width/10, self.height/20*19), PURPLE, self.text.big_font)
         self.defense_text = self.text.text_by_top_left("Defense " + str(stats_dict['defense']), (self.width/10, self.height/20*3), D_BLUE, self.text.small_font)
-        self.attack_text = self.text.text_by_top_left("Attack " + str(stats_dict['attack']), (self.width/10 + 1.2 * self.defense_text[1].width, self.height/20*3), RED_ORANGE, self.text.small_font)
+        self.attack_text = self.text.text_by_top_left("Attack " + str(stats_dict['attack']), (self.width/10 + 1.2 * self.defense_text[1].width, self.defense_text[1].y), RED_ORANGE, self.text.small_font)
+        self.strength_text = self.text.text_by_top_left("Strenght " + str(stats_dict['strength']), (self.attack_text[1].x + 1.2 * self.attack_text[1].width, self.attack_text[1].y), A_BLUE, self.text.small_font)
         self.render_list.append(self.level_text)
         self.render_list.append(self.defense_text)
         self.render_list.append(self.attack_text)
